6aa7751a5900e68c9d965707166fa83381a4b8c1
Joey <joeyjake987@gmail.com>
2023-04-14 13:06:17 -0400

    examples

--- Create A Task Python Project/workoutMain.py
+++ Create A Task Python Project/workoutMain.py
@@ -1,14 +1,16 @@
 import PySimpleGUI as GUI
 import time as T
-from window import run
+from defaultWindow import run
+from testWindow import testWindow
 
 def main():
     print("Booting Health Fit 100 App... ")
     bootWinLayout = [[GUI.Text("Booting Health Fit 100 App.... ")]]
     bootWindow = GUI.Window("Health Fit 100", bootWinLayout, margins=(50, 50)).read()
-    T.sleep(2)
+    T.sleep(1)
     bootWindow.close
-    run()
+    # run()
+    # testWindow()
 
 if __name__ == "__main__":
     main()
