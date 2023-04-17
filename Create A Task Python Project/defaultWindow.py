6238911094428ea68ab820424521fb978a1b16c8
Joey <joeyjrelli3824@gmail.com>
2023-04-17 09:48:36 -0400

    Workday2

--- Create A Task Python Project/defaultWindow.py
+++ Create A Task Python Project/defaultWindow.py
@@ -1,22 +1,28 @@
 import PySimpleGUI as GUI
 
 def run():
-    # Add a touch of color
-    GUI.theme('DarkAmber')  
+    # Add color to the GUI
+    GUI.theme('DarkBlue14')
      
-    # All the stuff inside your window.
-    layout = [  [GUI.Text('Some text on Row 1')],
-                [GUI.Text('Enter something on Row 2'), GUI.InputText()],
-                [GUI.Button('Ok'), GUI.Button('Cancel')] ]
+    # creates the window layout object
+    mainWindowLayout = [[GUI.Text("Some text on Row 1")], # Text
+                        [GUI.Text("Enter something on Row 2"), GUI.InputText()], # input text
+                        [GUI.Text("Enter something on Row 3"), GUI.InputText()], # input text
+                        [GUI.Input(key='-IN-', size=(20,1)), GUI.CalendarButton("Click to pop out calendar", close_when_date_chosen=True,  target='-IN-', location=(0,0), no_titlebar=False,)],
+                        [GUI.Button("Ok"), GUI.Button("Cancel")] ] # buttons
 
-    # Create the Window
-    window = GUI.Window('Window Title', layout)
+    # Initialize the window object and pass in our layout object
+    # This actually creates the window.
+    window = GUI.Window("Window Title", mainWindowLayout)
     
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
-        if event == GUI.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        if event == GUI.WIN_CLOSED or event == "Cancel": # if user closes window or clicks cancel
             break
-        print('You entered ', values[0])
+        row2Input = values[0]
+        row3Input = values[1]
+        calendarInput = GUI.popup('You chose:', GUI.popup_get_date())
+        print("Calendar date selected" + calendarInput)
 
     window.close()
--- Create A Task Python Project/workoutMain.py
+++ Create A Task Python Project/workoutMain.py
@@ -1,16 +1,40 @@
+# Importing the Py Simple GUI module to create a Graphical User Interface
 import PySimpleGUI as GUI
-import time as T
+
+# importing my run method from my defaultWindow file
 from defaultWindow import run
-from testWindow import testWindow
 
 def main():
-    print("Booting Health Fit 100 App... ")
-    bootWinLayout = [[GUI.Text("Booting Health Fit 100 App.... ")]]
-    bootWindow = GUI.Window("Health Fit 100", bootWinLayout, margins=(50, 50)).read()
-    T.sleep(1)
-    bootWindow.close
-    # run()
-    # testWindow()
-
+    # Print line to console letting us know the app is booting
+    print("Launching Health Fit 100 App... ")
+    
+    # Add color to the GUI
+    GUI.theme('DarkBlue14')
+    
+    # Create the window layout object
+    bootWinLayout = [[GUI.Text("Launching Health Fit 100 App.... ")], # Text
+                     [GUI.Button(button_text = "START", auto_size_button = True)]] # Start button
+    
+    # Initialize the window object and pass in our layout object
+    # This actually creates the window.
+    bootWindow = GUI.Window("Health Fit 100", bootWinLayout, margins=(50, 50))
+    
+    # Infinite Loop which keeps our window open until a conditional is met that breaks us out of the loop
+    while True:
+        # assigns event and values variables to the data on our boot Window using the .read() method
+        event, values = bootWindow.read()
+        
+        # End the program if user closes window or presses the Start button
+        if event == "START" or event == GUI.WIN_CLOSED:
+            # closes window
+            bootWindow.close()
+            
+            # boots our run method which contains our main app page
+            run()
+            
+            # end while loop
+            break
+    
+# Main method
 if __name__ == "__main__":
     main()
