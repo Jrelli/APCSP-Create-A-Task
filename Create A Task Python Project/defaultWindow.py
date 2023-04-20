15a3f6d5af2ec7b5886ee16d83f939e836cb4513
Joey Borrelli <joeyjrelli3824@gmail.com>
2023-04-19 22:41:52 -0400

    Workday 3

--- Create A Task Python Project/defaultWindow.py
+++ Create A Task Python Project/defaultWindow.py
@@ -1,28 +1,50 @@
 import PySimpleGUI as GUI
 
 def run():
-    # Add color to the GUI
+    # lists to hold information relating to adding calories
+    foodDrinkNames = []
+    calories = []
+    foodEntryDates = []
+
+    # list to hold the distance traveled if the user does cardio
+    distanceTraveled = []
+
+    # list to hold the weight and reps if the use does weight training
+    workoutWeight = []
+    workoutReps = []
+    workoutEntryDates = []
+
+    # Set theme/color of window and its elements
     GUI.theme('DarkBlue14')
      
     # creates the window layout object
-    mainWindowLayout = [[GUI.Text("Some text on Row 1")], # Text
-                        [GUI.Text("Enter something on Row 2"), GUI.InputText()], # input text
-                        [GUI.Text("Enter something on Row 3"), GUI.InputText()], # input text
-                        [GUI.Input(key='-IN-', size=(20,1)), GUI.CalendarButton("Click to pop out calendar", close_when_date_chosen=True,  target='-IN-', location=(0,0), no_titlebar=False,)],
-                        [GUI.Button("Ok"), GUI.Button("Cancel")] ] # buttons
+    mainWindowLayout = [[GUI.Text("Select a Date and enter in your food and its corresponding calories.")], # Text 
+                        [GUI.Input(key="Calendar", size=(20,1)), GUI.CalendarButton("Click to pop out calendar", close_when_date_chosen=True,  target="Calendar", location=(0,0), no_titlebar=False,)],
+                        [GUI.Text("Enter the name of the food/drink: "), GUI.InputText()], # input text
+                        [GUI.Text("Enter the number of calories it is: "), GUI.InputText()], # input text
+                        [GUI.Button("Log Entry"), GUI.Button("Exit")]] # buttons
 
     # Initialize the window object and pass in our layout object
     # This actually creates the window.
-    window = GUI.Window("Window Title", mainWindowLayout)
+    window = GUI.Window("Health Fit 100 app", mainWindowLayout)
     
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
+        # assigns event and values variables to the data on our boot Window using the .read() method
         event, values = window.read()
-        if event == GUI.WIN_CLOSED or event == "Cancel": # if user closes window or clicks cancel
+        
+        # prints our values for testing purposes
+        print(event, values)
+        
+        # checks conditions (if user closes window or clicks exit) to see if we should end our loop
+        if event == GUI.WIN_CLOSED or event == "Exit":
             break
-        row2Input = values[0]
-        row3Input = values[1]
-        calendarInput = GUI.popup('You chose:', GUI.popup_get_date())
-        print("Calendar date selected" + calendarInput)
 
+        # appends our lists if an entry is logged
+        if event == "Log Entry":
+            foodDrinkNames.append(values[0])
+            calories.append(values[1])
+            calendarInput = values["Calendar"]
+
+    # closes window
     window.close()
--- Create A Task Python Project/workoutMain.py
+++ Create A Task Python Project/workoutMain.py
@@ -4,20 +4,27 @@ import PySimpleGUI as GUI
 # importing my run method from my defaultWindow file
 from defaultWindow import run
 
+# Method to set the size and position of the colum, this is used mainly to center the column on the page.
+def ColumnFixedSize(layout, size=(None, None), *args, **kwargs):
+    # An addition column is needed to wrap the column with the Sizers because the colors will not be set on the space the sizers take
+    return GUI.Column([[GUI.Column([[GUI.Sizer(0,size[1]-1), GUI.Column([[GUI.Sizer(size[0]-2,0)]] + layout, *args, **kwargs, pad=(0,0))]], *args, **kwargs)]],pad=(0,0))
+
 def main():
     # Print line to console letting us know the app is booting
     print("Launching Health Fit 100 App... ")
     
-    # Add color to the GUI
+    # Set theme/color of window and its elements
     GUI.theme('DarkBlue14')
+
+    columnElements = [[GUI.Button(button_text = "START", auto_size_button = True)]] # Start button element
     
     # Create the window layout object
-    bootWinLayout = [[GUI.Text("Launching Health Fit 100 App.... ")], # Text
-                     [GUI.Button(button_text = "START", auto_size_button = True)]] # Start button
+    bootWinLayout = [[GUI.Text("Launching Health Fit 100 App.... ")],
+                     [ColumnFixedSize(columnElements, size=(200, 50), element_justification='c', vertical_alignment='t')]]
     
     # Initialize the window object and pass in our layout object
     # This actually creates the window.
-    bootWindow = GUI.Window("Health Fit 100", bootWinLayout, margins=(50, 50))
+    bootWindow = GUI.Window("Health Fit 100 booter...", bootWinLayout, margins=(100, 10))
     
     # Infinite Loop which keeps our window open until a conditional is met that breaks us out of the loop
     while True:
